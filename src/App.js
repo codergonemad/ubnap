67853d7fbf6175a6564064ad544f73c3e1bb8a1f
Saket Singh <saketsingh@protonmail.com>
2024-09-28 14:30:01 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React from 'react';
-   import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-   import Header from './components/Header';
-   import Footer from './components/Footer';
-   import HomePage from './pages/HomePage';
-   import ProductsPage from './pages/ProductsPage';
-   import ContactPage from './pages/ContactPage';
-   import DeveloperCredits from './components/DeveloperCredits';
-   import ProductDetailPage from './pages/ProductDetailPage';
-   import ThirdPartyManufacturingPage from './pages/ThirdPartyManufacturingPage';
-   import PCDFranchisePage from './pages/PCDFranchisePage';
-   import ScrollToTop from './components/ScrollToTop';
-   import './App.css';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import ContactPage from './pages/ContactPage';
+import DeveloperCredits from './components/DeveloperCredits';
+import ProductDetailPage from './pages/ProductDetailPage';
+import ThirdPartyManufacturingPage from './pages/ThirdPartyManufacturingPage';
+import PCDFranchisePage from './pages/PCDFranchisePage';
+import ScrollToTop from './components/ScrollToTop';
+import './App.css';
 
-   function App() {
-     return (
-      <Router basename="/ubnap"> 
-         <ScrollToTop />
-         <div className="App">
-           <Header />
-           <main>
-             <Routes>
-               <Route path="/" element={<HomePage />} />
-               <Route path="/products" element={<ProductsPage />} />
-               <Route path="/contact" element={<ContactPage />} />
-               <Route path="/developer" element={<DeveloperCredits />} />
-               <Route path="/product/:id" element={<ProductDetailPage />} />
-               <Route path="/third-party-manufacturing" element={<ThirdPartyManufacturingPage />} />
-               <Route path="/pcd-franchise" element={<PCDFranchisePage />} />
-             </Routes>
-           </main>
-           <Footer />
-         </div>
-       </Router>
-     );
-   }
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/developer" element={<DeveloperCredits />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
+            <Route path="/third-party-manufacturing" element={<ThirdPartyManufacturingPage />} />
+            <Route path="/pcd-franchise" element={<PCDFranchisePage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
-   export default App;
+export default App;
